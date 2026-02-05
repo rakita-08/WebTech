934ddc6c22b25f7a3dd00793d06ead49b0205de1
rakita-08 <rakitarimal@gmail.com>
2026-02-05 21:32:20 +0545

    connecting db in login and validating the user

--- flowrify/login_process.php
+++ flowrify/login_process.php
@@ -4,12 +4,31 @@ if(empty($_POST["username"]) | empty($_POST["password"])){
 }
 
 $uname = $_POST["username"];
-$pw = $_POST["password"];
+$pass = $_POST["password"];
 
+/*
 if($uname == "rakeeta" & $pw == "rakeee"){
     header("Location:dashboard.php");
 } else{
     header("Location:login.php");
-}
+} */
 
+$host = "localhost";
+$user = "rakeeta";
+$pw = "rakeee";
+$db = "flowrify";
+
+$con = mysqli_connect($host, $user, $pw, $db);
+if(!$con){
+    die("Error" . mysqli_connect_error());
+}
+$query = "select * from user_data where Email = '$uname'";
+$result = mysqli_query($con, $query);
+$row = mysqli_fetch_assoc($result);
+
+if(password_verify($pass, $row["Password"])){
+    header("Location:dashboard.php");
+} else{
+    header("Location:login.php");
+}
 ?>
