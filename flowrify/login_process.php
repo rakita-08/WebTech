4b01f6baba97e8c11dc9aa9b572bc2b7324f253a
rakita-08 <rakitarimal@gmail.com>
2026-02-07 13:50:00 +0545

    session added

--- flowrify/login_process.php
+++ flowrify/login_process.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 if(empty($_POST["username"]) | empty($_POST["password"])){
     header("Location:login.php");
 }
@@ -27,6 +29,7 @@ $result = mysqli_query($con, $query);
 $row = mysqli_fetch_assoc($result);
 
 if(password_verify($pass, $row["Password"])){
+    $_SESSION["username"] = $uname;
     header("Location:dashboard.php");
 } else{
     header("Location:login.php");
